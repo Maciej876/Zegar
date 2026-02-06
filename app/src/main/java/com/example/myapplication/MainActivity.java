995e67b973ba913dc8fce7d8c51df6f397706904
Maciej876 <maciek.falek4@gmail.com>
2026-02-06 10:15:01 +0100

    TrocheDziala

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TimePicker timePicker;
+    DatePicker dataPicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        button = findViewById(R.id.button);
+        timePicker = findViewById(R.id.czas);
+        dataPicker = findViewById(R.id.data);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int godzina = timePicker.getHour();
+                        int minuta = timePicker.getMinute();
+                        int rok = dataPicker.getYear();
+                        Toast.makeText(MainActivity.this, "godzina: "+godzina+" minuta: "+minuta+" rok: "+rok, Toast.LENGTH_SHORT).show();
+                        Date data = new Date();
+                        Toast.makeText(MainActivity.this, ""+data, Toast.LENGTH_SHORT).show();
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.YEAR, dataPicker.getYear());
+                        calendar.set(Calendar.MINUTE, dataPicker.getYear());
+                        Toast.makeText(MainActivity.this, ""+calendar.get(Calendar.YEAR), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
         });
     }
 }
